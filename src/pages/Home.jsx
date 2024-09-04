75a4815c4c093202b36ede66e113b942912c68f6
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-09-03 22:50:37 -0300

    feat: melhores no toggle de negativo e localstorage para o tamanho do calculo e para o negativo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function Home() {
-  const [input, setInput] = useState(100);
-  const [negativo, setNegativo] = useState(false);
+  const [size, setSize] = useState(() => {
+    return localStorage.getItem("calcSize") || 100;
+  });
+  const [negative, setNegative] = useState(() => {
+    const storedValue = localStorage.getItem("isNegative");
+    return storedValue === "true";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("isNegative", negative.toString());
+    localStorage.setItem("calcSize", size);
+  }, [negative, size]);
+
+  const calcSize = (event) => {
+    localStorage.setItem("calcSize", event.target.value);
+
+    setSize(event.target.value);
+  };
+
+  function Negative() {
+    const toggleChecked = () => {
+      setNegative((prevState) => !prevState);
+    };
+
+    return (
+      <>
+        <button
+          onClick={toggleChecked}
+          className="flex flex-row gap-2 justify-between items-center w-full h-12 p-3 bg-slate-200 rounded-md cursor-pointer"
+        >
+          Números negativos
+          <i
+            className={
+              negative
+                ? "fa-solid fa-toggle-on text-2xl"
+                : "fa-solid fa-toggle-off text-2xl"
+            }
+          ></i>
+        </button>
+      </>
+    );
+  }
 
   function LinkButton({ text, type }) {
     return (
       <Link
         to="/play"
-        state={{ type: type, negativo: negativo, maximo: input }}
+        state={{ type: type, negativo: negative, maximo: size }}
         className="w-full h-14 flex justify-center items-center text-white rounded-lg bg-blue-500 hover:bg-blue-800 active:bg-blue-700"
       >
         {text}
       </Link>
-    );
-  }
-
-  function Negativo() {
-    return (
-      <button onClick={() => setNegativo(!negativo)}>
-        Números negativos: {String(negativo)}
-      </button>
     );
   }
 
@@ -31,12 +64,12 @@ export default function Home() {
         Valor máximo:
         <input
           className="w-full h-20 rounded-lg border-4 text-center text-xl"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          type="text"
+          value={size}
+          onChange={calcSize}
+          type="number"
           inputMode="numeric"
         />
-        <Negativo />
+        <Negative />
         <LinkButton text="Soma" type="soma" />
         <LinkButton text="Subtração" type="subt" />
         <LinkButton text="Multiplicação" type="mult" />
