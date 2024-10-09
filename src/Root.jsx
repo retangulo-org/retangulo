a5f67d1311f65be5b1d53b773dc583e08d951cda
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-10-09 12:35:40 -0300

    feat: footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import { Analytics } from "@vercel/analytics/react"
 
 export default function Root() {
   return (
     <>
       <Header />
-      <div className="flex justify-center w-full p-5">
-        <div className="w-full sm:max-w-2xl md:max-w-4xl lg:max-w-6xl">
+      <main className="flex justify-center w-full p-5 bg-white dark:bg-black">
+        <div className="w-full sm:max-w-2xl">
           <Outlet />
-          {/* <Footer /> */}
         </div>
-      </div>
+      </main>
+      <Footer />
       <Analytics />
     </>
   );
