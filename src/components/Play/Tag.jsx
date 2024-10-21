eb6c10cb9b5cb4f38e33479f4b3ae45fc66d62d1
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-10-21 20:44:28 -0300

    style: aumenta o padding do componente `Tag`

--- src/components/Play/Tag.jsx
+++ src/components/Play/Tag.jsx
@@ -1,27 +1,27 @@
 export default function Tag({ texto, tipo }) {
-	if (tipo == 'pontos') {
-		return (
-			<span className="p-1 bg-green-500 font-bold text-white space-x-6 rounded leading-none">
-				<i className="fa-solid fa-check"></i> {texto}
-			</span>
-		);
-	} else if (tipo == 'erros') {
-		return (
-			<span className="p-1 bg-red-500 font-bold text-white rounded leading-none">
-				<i className="fa-solid fa-xmark"></i> {texto}
-			</span>
-		);
-	} else if (tipo == 'anterior') {
-		return (
-			<span className="p-1 bg-blue-500 font-bold text-white rounded leading-none">
-				<i className="fa-solid fa-arrow-left"></i> {texto}
-			</span>
-		);
-	} else if (tipo == 'time') {
-		return (
-			<span className="p-1 bg-orange-500 font-bold text-white rounded leading-none">
-				<i className="fa-solid fa-clock"></i> {texto}
-			</span>
-		);
-	}
+  if (tipo == 'pontos') {
+    return (
+      <span className="p-2 bg-green-500 font-bold text-white space-x-6 rounded leading-none">
+        <i className="fa-solid fa-check"></i> {texto}
+      </span>
+    );
+  } else if (tipo == 'erros') {
+    return (
+      <span className="p-2 bg-red-500 font-bold text-white rounded leading-none">
+        <i className="fa-solid fa-xmark"></i> {texto}
+      </span>
+    );
+  } else if (tipo == 'anterior') {
+    return (
+      <span className="p-2 bg-blue-500 font-bold text-white rounded leading-none">
+        <i className="fa-solid fa-arrow-left"></i> {texto}
+      </span>
+    );
+  } else if (tipo == 'time') {
+    return (
+      <span className="p-2 bg-orange-500 font-bold text-white rounded leading-none">
+        <i className="fa-solid fa-clock"></i> {texto}
+      </span>
+    );
+  }
 }
