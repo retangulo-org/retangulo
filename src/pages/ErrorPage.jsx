4dc858be609dd776cdc9e5cc83057ba8fef1b25d
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-10-21 10:15:34 -0300

    style: atualiza a página de erro 404

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,19 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
 export default function ErrorPage() {
   return (
-      <div className="flex flex-col w-full h-[80vh] gap-5 justify-center items-center text=black dark:text-white">
-        <h1 className="text-7xl font-bold">Erro 404</h1>
-        <a href="/" className="w-full p-4 bg-blue-600 text-white text-center rounded-md">Menu principal</a>
-      </div>
-  )
+    <>
+      <Header />
+      <main className="flex justify-center w-full p-5 bg-neutral-100 dark:bg-neutral-950">
+        <div className="flex flex-col w-full sm:max-w-3xl h-[80vh] gap-5 justify-center items-center text=black dark:text-white">
+          <h1 className="text-7xl font-bold">Erro 404</h1>
+          <a href="/" className="w-full p-4 bg-blue-600 text-white text-center rounded-md">
+            Menu principal
+          </a>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
