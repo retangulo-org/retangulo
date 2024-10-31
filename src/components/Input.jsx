69304f18cdaf392294cfc77fe711b4280c051c58
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-10-30 22:41:44 -0300

    style: melhorias no visual do site

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
-export default function Input({ onChange, value, placeholder, autoFocus, span }) {
+export default function Input({ onChange, value, placeholder, autoFocus }) {
   return (
     <div className="flex flex-col w-full">
-      <h3 className="py-2 text-black dark:text-white text-base font-bold select-none">{span}</h3>
-
       <input
         className="w-full h-16 border-2 bg-neutral-200 dark:bg-neutral-800 border-black/20 dark:border-white/20 px-4 py-2 text-black dark:text-white focus:outline-none rounded-md text-center font-bold text-2xl hover:ring-2 hover:ring-blue-500"
         value={value}
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,25 +1,25 @@
 export default function Tag({ texto, tipo }) {
   if (tipo == 'pontos') {
     return (
-      <span className="p-2 bg-green-500 font-bold text-white space-x-6 rounded leading-none">
+      <span className="p-2 bg-green-500 font-bold text-neutral-200 text-center space-x-6 rounded leading-none">
         <i className="fa-solid fa-check"></i> {texto}
       </span>
     );
   } else if (tipo == 'erros') {
     return (
-      <span className="p-2 bg-red-500 font-bold text-white rounded leading-none">
+      <span className="p-2 bg-red-500 font-bold text-neutral-200 text-center rounded leading-none">
         <i className="fa-solid fa-xmark"></i> {texto}
       </span>
     );
   } else if (tipo == 'anterior') {
     return (
-      <span className="p-2 bg-blue-500 font-bold text-white rounded leading-none">
+      <span className="p-2 bg-blue-500 font-bold text-neutral-200 text-center rounded leading-none">
         <i className="fa-solid fa-arrow-left"></i> {texto}
       </span>
     );
   } else if (tipo == 'time') {
     return (
-      <span className="p-2 bg-orange-500 font-bold text-white rounded leading-none">
+      <span className="p-2 bg-orange-500 font-bold text-neutral-200 text-center rounded leading-none">
         <i className="fa-solid fa-clock"></i> {texto}
       </span>
     );
