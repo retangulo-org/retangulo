538b9b563e63de89aa6aed29a19f05576ce6c78c
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-11-03 10:00:51 -0300

    fix: corrige o favicon

--- vite.config.js
+++ vite.config.js
@@ -29,12 +29,11 @@ export default defineConfig({
         orientation: 'natural',
         icons: [
           {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
           },
           {
-            src: '/android-chrome-512x512.png',
+            src: '/android-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
