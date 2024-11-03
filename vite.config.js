5fbb70698fc380955bd7ab1af846510ff193f2e0
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-11-03 09:09:36 -0300

    fix(pwa): corrige a rotação automática do PWA

--- vite.config.js
+++ vite.config.js
@@ -22,11 +22,11 @@ export default defineConfig({
         description:
           'O Retangulo.org é uma plataforma de exercícios matemáticos projetada para desafiar suas habilidades de cálculo. Melhore suas habilidades de resolução de cálculo mental e resolva as questões o mais rápido possível sem cometer erros. Atualmente, o Retangulo.org, conta com a possibilidade de gerar cálculos aleatórios com ou sem negativo nas seguintes operações matemáticas: adição, subtração, divisão, multiplicação, raiz quadrada, expoente 2 e expoente 3.',
         display: 'standalone',
-        id: '/',
+        id: '/jogar',
         lang: 'ptbr',
-        start_url: '/',
+        start_url: '/jogar',
         theme_color: '#2563EB',
-        orientation: 'any',
+        orientation: 'natural',
         icons: [
           {
             src: '/android-chrome-192x192.png',
