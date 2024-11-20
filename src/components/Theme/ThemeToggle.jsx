cc3849f529e65eb3a11a504fd31035adf090c975
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-11-20 16:01:10 -0300

    fix: corrige o toggle de tema

--- src/components/Theme/ThemeToggle.jsx
+++ src/components/Theme/ThemeToggle.jsx
@@ -4,20 +4,54 @@ import { Moon, Sun } from 'lucide-react';
 import Button from '../Button';
 
 export default function DarkModeToggle() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isDark, setIsDark] = useState(() => {
+    if (localStorage.getItem('vite-ui-theme') === '' ) {
+      return false
+    }
+
+    if (localStorage.getItem('vite-ui-theme') === 'light' ) {
+      return false
+    }
+
+    if (localStorage.getItem('vite-ui-theme') === 'dark' ) {
+      return true
+    }
+  });
   const { setTheme } = useTheme();
 
   useEffect(() => {
     if (localStorage.getItem('vite-ui-theme') === 'light') {
-      setTheme('dark')
-    } else {
-      setTheme('light')    
+      setIsDark(false)
+      return setTheme('light')
     }
-  }, [isOpen])
+
+    if (localStorage.getItem('vite-ui-theme') === 'dark') {
+      setIsDark(true)
+      return setTheme('dark')
+    }
+
+    if (localStorage.getItem('vite-ui-theme') === '') {
+      setIsDark(false)
+      return setTheme('light')
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isDark === false) {
+      setIsDark(false)
+      return setTheme('light')
+    }
+
+    if (isDark) {
+      setIsDark(true)
+      return setTheme('dark')
+    }
+
+  }, [isDark])
 
   return (
-    <Button size="icon" onClick={() => setIsOpen(!isOpen)}>
-      {localStorage.getItem('vite-ui-theme') === 'light' ? <Sun /> : <Moon />}
+    <Button size="icon" onClick={() => setIsDark(!isDark)}>
+      {isDark ? <Moon /> : <Sun />}
     </Button>
   );
 }
