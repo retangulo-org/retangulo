917f7518523d987fb6b45ce50de7e7bfca73ee87
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-11-22 13:10:22 -0300

    refactor: remove o componente radio

--- src/components/Radio.jsx
+++
@@ -1,8 +0,0 @@
-export default function Radio({ label, value, checked, onChange }) {
-  return (
-    <label className="flex flex-row gap-4 text-text select-none">
-      <input type="radio" value={value} checked={checked} onChange={onChange} />
-      {label}
-    </label>
-  );
-}
