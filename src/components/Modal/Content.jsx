e3dbc728baf121318e6f7b80c278f412d97abbf1
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-11-24 16:25:26 -0300

    css: adiciona o prop className ao componente modal content

--- src/components/Modal/Content.jsx
+++ src/components/Modal/Content.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function Content({ children }) {
-  return <div className="flex flex-col mb-6">{children}</div>;
+export default function Content({ children, className }) {
+  return <div className={`${className} flex flex-col mb-6`}>{children}</div>;
 }
