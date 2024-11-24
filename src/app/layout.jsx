9b838c8aa194d4c289806cdddf1fbee5e462e1d9
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-11-24 16:26:17 -0300

    css: melhora a interface geral

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export default function Root() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <Header />
-      <main className="flex justify-center w-full p-5 bg-neutral-100 dark:bg-neutral-950">
+      <main className="flex justify-center w-full p-4 bg-neutral-100 dark:bg-neutral-950">
         <div className="w-full sm:max-w-[720px]">
           <Outlet />
         </div>
