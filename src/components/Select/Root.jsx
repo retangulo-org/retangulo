bfad65bd8f5fe973b23e94357fcb8ea00f53a98b
Leydson Andrey <andrey.tar.xz@gmail.com>
2025-08-07 22:36:52 -0300

    fix: teclado mobile sem menos #67

--- src/components/Select/Root.jsx
+++ src/components/Select/Root.jsx
@@ -1,11 +1,11 @@
 import { ChevronDown } from 'lucide-react';
 
-export default function Root({ children, value, onChange }) {
+export default function Root({ children, className, childrenClassName, value, onChange }) {
   return (
-    <div className="w-full relative flex flex-row justify-center items-center hover:shadow-md">
+    <div className={`${className} w-full relative flex flex-row justify-center items-center hover:shadow-md`}>
       <ChevronDown className="absolute right-4 text-textAlt pointer-events-none" />
       <select
-        className="appearance-none actionDefault w-full h-10 rounded-sm cursor-pointer font-semibold text-center select-none"
+        className={`${childrenClassName} appearance-none actionDefault w-full h-10 rounded-sm cursor-pointer font-semibold text-center select-none`}
         value={value}
         onChange={onChange}>
         {children}
--- src/pages/Options.jsx
+++ src/pages/Options.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react';
 import { ArrowUpRight, Trash2 } from 'lucide-react';
+import { Select } from '../components/Select';
 import Button from '../components/ui/Button';
 import Meta from '../components/Meta';
 
@@ -9,10 +11,18 @@ const links = [
 ];
 
 export default function Options() {
+  const [keyType, setKeyType] = useState(() => {
+    return localStorage.getItem('keyType') || 'number';
+  });
+
+  useEffect(() => {
+    localStorage.setItem('keyType', keyType);
+  }, [keyType]);
+
   return (
     <Meta title="Opções — Gerador de Cálculos Matemáticos — Retangulo.org" canonical="https://retangulo.org/options">
       <div className="flex flex-col justify-center items-center">
-        <div className="w-full divide-neutral-200 dark:divide-neutral-800 divide-y-2">
+        <div className="w-full divide-foreground dark:divide-neutral-800 divide-y-2">
           <div className="w-full py-2 flex flex-row justify-between items-center">
             <h3 className="mb-0 !text-text">Limpar cache</h3>
             <Button
@@ -26,9 +36,34 @@ export default function Options() {
               <Trash2 />
             </Button>
           </div>
+          <div className="w-full py-2 flex flex-col ">
+            <div className="w-full pb-1 flex flex-row justify-between items-start">
+              <h3 className="mb-0 !text-text">
+                Teclado <span className="text-sm">(Dispositivos móveis)</span>
+              </h3>
+              <Select.Root
+                value={keyType}
+                className="!w-auto"
+                childrenClassName="!text-left !pl-4 !w-36"
+                onChange={(event) => {
+                  setKeyType(event.target.value);
+                }}>
+                {[
+                  ['Normal', 'text'],
+                  ['Numérico', 'number'],
+                ].map(([tempo_title, tempo]) => (
+                  <Select.Content value={tempo} option={tempo_title} />
+                ))}
+              </Select.Root>
+            </div>
+            <h6>
+              Altera o tipo de teclado virtual: normal (teclado padrão) ou numérico. Alguns teclados numéricos não
+              possuem o sinal de menos (-), isso impossibilita responder com números negativos.
+            </h6>
+          </div>
         </div>
         <h2 className="mt-8">Links</h2>
-        <div className="w-full divide-neutral-200 dark:divide-neutral-800 divide-y-2">
+        <div className="w-full divide-foreground divide-y-2">
           {links.map((link) => (
             <div key={link.id} className="w-full flex flex-row justify-between items-center">
               <a
