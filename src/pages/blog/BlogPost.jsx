c14b2b1b6ed12f27290d64f58aa2bda1c83e0ee8
Leydson Andrey <andrey.tar.xz@gmail.com>
2025-10-04 14:16:17 -0300

    feat: blog com todos os recursos padrões funcionando

--- src/pages/blog/BlogPost.jsx
+++ src/pages/blog/BlogPost.jsx
@@ -1,15 +1,65 @@
 import { useParams } from 'react-router-dom';
-import MDXWrapper from '../../components/MDXWrapper';
 import { posts } from '../../scripts/postsLoader';
 import Button from '../../components/ui/Button';
 import { useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
 import { ArrowLeft } from 'lucide-react';
+import Meta from '../../components/Meta';
+
+const mdxComponents = {
+  a: ({ children, ...props }) => (
+    <a className="text-primary hover:underline" {...props}>
+      {children}
+    </a>
+  ),
+  h2: ({ children, ...props }) => (
+    <h2 className="text-2xl font-bold mt-6 mb-3" {...props}>
+      {children}
+    </h2>
+  ),
+  h3: ({ children, ...props }) => (
+    <h3 className="text-xl font-bold mt-4 mb-2" {...props}>
+      {children}
+    </h3>
+  ),
+  p: ({ children, ...props }) => (
+    <p className="mb-4 leading-relaxed" {...props}>
+      {children}
+    </p>
+  ),
+  ul: ({ children, ...props }) => (
+    <ul className="list-disc ml-6 mb-4" {...props}>
+      {children}
+    </ul>
+  ),
+  ol: ({ children, ...props }) => (
+    <ol className="list-decimal ml-6 mb-4" {...props}>
+      {children}
+    </ol>
+  ),
+  li: ({ children, ...props }) => (
+    <li className="mb-2" {...props}>
+      {children}
+    </li>
+  ),
+  strong: ({ children, ...props }) => (
+    <strong className="text-neutral-300 font-extrabold" {...props}>
+      {children}
+    </strong>
+  ),
+  img: ({ children, ...props }) => (
+    <img className="mb-4 rounded-md text-center mx-auto" {...props}>
+      {children}
+    </img>
+  ),
+  code: ({ children, ...props }) => (
+    <code className="bg-gray-100 px-1 py-0.5 rounded text-sm" {...props}>
+      {children}
+    </code>
+  ),
+};
 
 export default function BlogPost() {
   const navigate = useNavigate();
-  const { i18n } = useTranslation();
-  const currentLang = i18n.language.split('-')[0];
 
   const { slug } = useParams();
   const post = posts.find((p) => p.slug === slug);
@@ -19,21 +69,28 @@ export default function BlogPost() {
   const Component = post.component;
 
   return (
-    <div className="mx-auto p-4 flex flex-col gap-8">
-      <div>
-        <Button className="!w-auto" variant="outline" onClick={() => navigate(`/${currentLang}/blog`)}>
-          <ArrowLeft /> Voltar
-        </Button>
+    <Meta
+      title={`${post.title} — Gerador de Cálculos Matemáticos — Retangulo.org`}
+      canonical={`https://retangulo.org/blog/${slug}`}
+      desc="Resolva cálculos matemáticos aleatórios & melhore seu cálculo mental.">
+      <div className="mx-auto p-4 flex flex-col gap-8">
+        <div>
+          <Button className="!w-auto" variant="outline" onClick={() => navigate('/blog')}>
+            <ArrowLeft /> Voltar
+          </Button>
+        </div>
+        <div>
+          <h1>{post.title}</h1>
+          <div className="flex flex-row gap-2">
+            <p>{post.date}</p>
+            <p>•</p>
+            <p>{post.autor}</p>
+          </div>
+        </div>
+        <div>
+          <Component components={mdxComponents} />
+        </div>
       </div>
-      <div>
-        <h1>{post.title}</h1>
-        <p>{post.date}</p>
-      </div>
-      <div>
-        <MDXWrapper>
-          <Component />
-        </MDXWrapper>
-      </div>
-    </div>
+    </Meta>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
 
 export default defineConfig({
-  plugins: [react(), mdx()],
+  plugins: [{ enforce: 'pre', ...mdx() }, react()],
 });
