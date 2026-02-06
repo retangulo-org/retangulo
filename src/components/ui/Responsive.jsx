92a2f742c9c0a1f5e8dd88d8aaa2410e0abc8d08
Leydson Andrey <andrey.tar.xz@gmail.com>
2026-02-05 23:28:29 -0300

    css: diminui o tamanho para modo responsivo mobile

--- src/components/ui/Responsive.jsx
+++ src/components/ui/Responsive.jsx
@@ -1,15 +1,15 @@
 import { useMediaQuery } from 'react-responsive';
 
 const Desktop = ({ children }) => {
-  const isDesktop = useMediaQuery({ minWidth: 992 });
+  const isDesktop = useMediaQuery({ minWidth: 769 });
   return isDesktop ? children : null;
 };
 const Mobile = ({ children }) => {
-  const isMobile = useMediaQuery({ maxWidth: 991 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
   return isMobile ? children : null;
 };
 const Default = ({ children }) => {
-  const isNotMobile = useMediaQuery({ minWidth: 768 });
+  const isNotMobile = useMediaQuery({ minWidth: 767 });
   return isNotMobile ? children : null;
 };
 
